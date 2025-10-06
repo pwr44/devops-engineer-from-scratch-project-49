05795461a5d5f5161ba4db9250beb96fd46704e3
Pavel Egorov <epe.mw@mail.ru>
2025-10-06 18:55:31 +0300

    Update brain-even.py

--- brain_games/scripts/games/brain_even.py
+++ brain_games/scripts/games/brain_even.py
@@ -3,10 +3,12 @@ from random import randint
 from brain_games.engine import engine
 
 GAME_TASK: str = 'Answer "yes" if the number is even, otherwise answer "no".'
+MIN_NUMBER = 1
+MAX_NUMBER = 10
 
 
 def get_num():
-    return randint(1, 10)
+    return randint(MIN_NUMBER, MAX_NUMBER)
 
 
 def is_even(num):
